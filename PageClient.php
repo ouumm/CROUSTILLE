130eb64c4a8ba1cb4ab8b7bddd8f460ed9458373
isradh <128903699+isradh@users.noreply.github.com>
2024-01-05 12:54:28 +0100

    liaison entre les pages, revu design

--- PageClient.php
+++ PageClient.php
@@ -15,7 +15,7 @@ if(!$_SESSION['mdp']){
     <link rel="stylesheet" href="stylePC.css">
     <link href='https://fonts.googleapis.com/css?family=Varela Round' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=Rethink Sans' rel='stylesheet'>
-    <lin
+
 
 
 </head>
@@ -25,8 +25,9 @@ if(!$_SESSION['mdp']){
     <?php
 
     if (isset($_SESSION['nom'])) {
+        $nomEnMajuscule = ucfirst(htmlspecialchars($_SESSION['nom']));
         echo '<h1>Ça Crous\'tille</h1>';
-        echo '<h2>Bienvenue ' . htmlspecialchars($_SESSION['nom']) . '</h2>';
+        echo '<h2>Bienvenue dans votre Espace client ' . $nomEnMajuscule  . ' ! </h2>' ;
     } else {
         echo '<h1>Ça Crous\'tille</h1>';
     }
@@ -34,6 +35,9 @@ if(!$_SESSION['mdp']){
 
     <div class="lien-deco">
         <a href="deconnexion.php?deco">Se déconnecter</a>
+    </div>
+    <div class="lien-deco">
+        <a href="accueil.html">Accueil</a>
     </div>
 
 </div>
@@ -46,10 +50,23 @@ if(!$_SESSION['mdp']){
         <h1> Mon espace client gourmand  </h1>
     </div>
 
-    <div class="conteneur">
-        <div class="forme"></div>
-        <div class="forme"></div>
-        <div class="forme"></div>
+    <div id="card-1" class="conteneur">
+        <div class="forme">
+            <a href="itineraire.html" class="ctn"  >
+                <h1> A proximité </h1>
+                <img src="images/logo/location.png" class="img">
+
+            </a></div>
+        <div class="forme" id="card-3">
+            <a href="proximiter.html" class="ctn">
+                <h1> Itinéraire </h1>
+                <img src="images/logo/itinerary.png" class="img">
+            </a></div>
+        <div class="forme" id="card-3">
+            <a href="forum.php" class="ctn">
+                <h1> Forum </h1>
+                <img src="images/logo/parler.png" class="img">
+            </a></div>
     </div>
 </div>
 
--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 $insertUser = null;
 
-    $bdd = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$bdd = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 
 
